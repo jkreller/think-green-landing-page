eb7958c2e20433e1a3a8f327348fe5f874231ef2
Julian Kreller <jkreller@eos-uptrade.de>
2020-10-12 18:54:29 +0200

    - Add minifying

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
+const minify = require('express-minify');
 
 const indexRouter = require('./routes/index');
 const database = require('./database');
@@ -23,8 +24,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600 }));
 app.use(compression());
+app.use(minify({cache: path.join(__dirname, 'minify-cache')}));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31557600}));
 
 app.use('/', indexRouter);
 
